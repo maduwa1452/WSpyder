521e381a0a4c6519a110dc3092ff5488c2dcfa07
maduwa1452 <dhythdjgdyjyj@gmail.com>
2025-04-04 17:07:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "EwYC0bRL#Xnb-F0ZaQ_k2MNsUL6028La8KggQv6PkAz7-g7P9JKs",
+  SESSION_ID: process.env.SESSION_ID || "cxRw2BjK#hhE9CQE6SPqjo7P8Esc9LycUrIXjQW3-SRlietAg9Gk",
   MONGODB: process.env.MONGODB || "mongodb://mongo:zSvJuKfbTAVKsGUcJdddCLujEhkCfNlc@shinkansen.proxy.rlwy.net:49840",
   OWNER_NUM: process.env.OWNER_NUM || "94724610079",
 };
