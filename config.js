7c9724472a83b34b306545c11f59b9ca0973e843
maduwa1452 <dhythdjgdyjyj@gmail.com>
2025-04-04 16:07:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
-  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
+  SESSION_ID: process.env.SESSION_ID || "EwYC0bRL#Xnb-F0ZaQ_k2MNsUL6028La8KggQv6PkAz7-g7P9JKs",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:zSvJuKfbTAVKsGUcJdddCLujEhkCfNlc@shinkansen.proxy.rlwy.net:49840",
+  OWNER_NUM: process.env.OWNER_NUM || "94724610079",
 };
